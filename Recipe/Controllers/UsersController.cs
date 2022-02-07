2935a7bf646036bdfa2e809388da0cb5a12f54a0
ceejayv328 <ceejayv328@gmail.com>
2022-02-07 19:02:04 +0800

    Added authenticate method (Created controller for users)

---
+++ Recipe/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Recipe.Models;
+using Recipe.Repositories.IRepositories;
+
+namespace Recipe.Controllers
+{
+    [Authorize]
+    [Route("api/v{version:apiVersion}/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] User model)
+        {
+            var user = _userRepository.Authenticate(model.Username, model.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+            return Ok(User);
+        }
+    }
+}
