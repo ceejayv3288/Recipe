877640f0f0e70a3602cbd116b2f809ef931dc2e7
ceejayv328 <ceejayv328@gmail.com>
2022-02-07 18:58:28 +0800

    Create token on successful authentication

--- Recipe/Repositories/UserRepository.cs
+++ Recipe/Repositories/UserRepository.cs
@@ -1,23 +1,59 @@
-﻿using Recipe.Models;
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Recipe.Data;
+using Recipe.Models;
 using Recipe.Repositories.IRepositories;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
 
 namespace Recipe.Repositories
 {
     public class UserRepository : IUserRepository
     {
-        public User Authenticate(string username, string passwork)
+        private readonly ApplicationDbContext _db;
+        private readonly AppSettings _appSettings;
+
+        public UserRepository(ApplicationDbContext db, IOptions<AppSettings> appsettings)
         {
-            throw new System.NotImplementedException();
+            _db = db;
+            _appSettings = appsettings.Value;
+        }
+
+        public User Authenticate(string username, string password)
+        {
+            var user = _db.Users.SingleOrDefault(x => x.Username == username && x.Password == password);
+
+            if (user == null)
+                return null;
+
+            //if user was found, generate a jwt token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[] {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+
+            return user;
         }
 
         public bool IsUniqueUser(string username)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public User Register(string username, string password)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
     }
 }
